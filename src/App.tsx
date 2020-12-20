c986186136d6a16936e773bdae83e1d0917e2005
ejdejesu <evan.dejesus@24g.com>
2020-12-20 17:10:48 -0500

    add proper hamburger menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,53 @@
 import React from "react";
 import ExternalLink from "./ExternalLink";
 import "./App.css";
+import hamburger from "./assets/hamburger.png";
 
-function App() {
+const MyLinks = () => {
+  return (
+    <>
+      <ExternalLink href="https://github.com/ejdejesu" text="GitHub" />
+      <ExternalLink
+        href="https://www.instagram.com/evandejesus_"
+        text="Instagram"
+      />
+      <ExternalLink href="https://ko-fi.com/evandejesus" text="Ko-fi" />
+    </>
+  );
+};
+
+const App = () => {
+  const [isMenuShowing, setShowMenu] = React.useState(false);
+
+  const toggleMenu = () => setShowMenu((m) => !m);
+
   return (
     <div className="App">
+      {isMenuShowing && (
+        <>
+          <div className="App-hamburger-background"></div>
+          <div className="App-hamburger-menu">
+            <MyLinks />
+          </div>
+        </>
+      )}
       <div className="App-navigation-wrapper">
         <div className="App-navigation">
           <div className="App-title">
             <div>Evan de Jesus</div>
           </div>
+          <div className="App-hamburger" onClick={toggleMenu}>
+            <img src={hamburger} alt="hamburger" />
+          </div>
           <div className="App-links-wrapper">
             <div className="App-links">
-              <ExternalLink href="https://github.com/ejdejesu" text="GitHub" />
-              <ExternalLink
-                href="https://www.instagram.com/evandejesus_"
-                text="Instagram"
-              />
-              <ExternalLink href="https://ko-fi.com/evandejesus" text="Ko-fi" />
+              <MyLinks />
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
