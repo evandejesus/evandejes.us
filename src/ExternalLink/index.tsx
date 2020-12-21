b6dc577a5da64b00a96856048f0b9139df46326d
ejdejesu <evan.dejesus@24g.com>
2020-12-20 20:28:39 -0500

    add blink pseudo-element

--- src/ExternalLink/index.tsx
+++ src/ExternalLink/index.tsx
@@ -1,9 +1,17 @@
 import React, { FunctionComponent } from "react";
 type ExternalLinkProps = { href: string; text: string };
 const ExternalLink: FunctionComponent<ExternalLinkProps> = ({ href, text }) => {
+  const [pink, setPink] = React.useState(false);
   return (
     <a
+      onTouchStart={() => {
+        setPink(true);
+      }}
+      onTouchEnd={() => {
+        setPink(false);
+      }}
       className="ExternalLink-link"
+      style={{ color: pink ? "#ef7997" : "FFF" }}
       target="_blank"
       rel="noopener noreferrer"
       href={href}
