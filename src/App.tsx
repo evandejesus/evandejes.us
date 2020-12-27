54777927725aedce8c3aa2b151c5069cdafe717d
ejdejesu <evan.dejesus@24g.com>
2020-12-26 21:18:08 -0500

    add content to resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { Link, Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import ExternalLink from "./ExternalLink";
 import TerminalText from "./TerminalText";
+import Resume from "./Resume";
 import hamburger from "./assets/hamburger.png";
 import close from "./assets/close.png";
 import "./App.css";
@@ -26,37 +27,46 @@ const App = () => {
 
   return (
     <Router>
-      <Switch>
-        <Route path="/resume"></Route>
-        <Route path="/">
-          <div className="App">
-            {isMenuShowing && (
-              <>
-                <div className="App-hamburger-background"></div>
-                <div className="App-hamburger-menu">
-                  <MyLinks />
-                </div>
-              </>
-            )}
-            <div className="App-navigation-wrapper">
-              <div className="App-navigation">
-                <div className="App-title">
-                  <div>Evan de Jesus</div>
-                </div>
-                <div className="App-hamburger" onClick={toggleMenu}>
-                  {isMenuShowing ? (
-                    <img src={close} alt="x" />
-                  ) : (
-                    <img src={hamburger} alt="=" />
-                  )}
-                </div>
-                <div className="App-links-wrapper">
-                  <div className="App-links">
-                    <MyLinks />
-                  </div>
-                </div>
+      <div>
+        {isMenuShowing && (
+          <>
+            <div className="App-hamburger-background"></div>
+            <div className="App-hamburger-menu">
+              <MyLinks />
+            </div>
+          </>
+        )}
+        <div className="App-navigation-wrapper">
+          <div className="App-navigation">
+            <div className="App-title">
+              <Link
+                to="/"
+                style={{ textDecoration: "none", color: "var(--black)" }}
+              >
+                <div>Evan de Jesus</div>
+              </Link>
+            </div>
+            <div className="App-hamburger" onClick={toggleMenu}>
+              {isMenuShowing ? (
+                <img src={close} alt="x" />
+              ) : (
+                <img src={hamburger} alt="=" />
+              )}
+            </div>
+            <div className="App-links-wrapper">
+              <div className="App-links">
+                <MyLinks />
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <Switch>
+        <Route path="/resume">
+          <Resume />
+        </Route>
+        <Route path="/">
+          <div className="App">
             <div className="App-landing-page">
               <div className="App-landing-page-content">
                 <TerminalText />
---
+++ src/Resume/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.css";
+
+const Resume = () => {
+  return (
+    <div className="Resume-container">
+      <div className="Resume-content">
+        <h1>Education</h1>
+        <h1>Certifications</h1>
+        <h1>Experience</h1>
+        <h1>Projects</h1>
+        <h1>Skills & Software</h1>
+        <h1>Activities</h1>
+      </div>
+    </div>
+  );
+};
+export default Resume;
