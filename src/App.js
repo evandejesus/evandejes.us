9f1caa9d29a5f2fc107a0a62d24aec7b0b796017
Evan de Jesus <evan@chess.com>
2023-04-02 21:29:45 -0600

    init mobile marquee

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+import MobileMarquee from "./components/MobileMarquee";
 
-function App() {
+const App = () => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+  const isBigScreen = useMediaQuery({ query: "(min-width: 1824px)" });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
+  const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
+
+  if (isTabletOrMobile) {
+    return (
+      <div>
+        {/* <p>You are a tablet or mobile phone</p> */}
+        <MobileMarquee></MobileMarquee>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Device Test!</h1>
+      {isDesktopOrLaptop && <p>You are a desktop or laptop</p>}
+      {isBigScreen && <p>You have a huge screen</p>}
+      <p>Your are in {isPortrait ? "portrait" : "landscape"} orientation</p>
+      {isRetina && <p>You are retina</p>}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/MobileMarquee/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+
+const MobileMarquee = () => {
+  return <div>fuck you</div>;
+};
+
+export default MobileMarquee;
